ea76f292d8d51311ca2dee1f6d54839770ed3506
Auguste Baum <auguste.apple@gmail.com>
2025-04-03 00:43:30 +0200

    shell.nix: Init

---
+++ shell.nix
@@ -0,0 +1,8 @@
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/ab7b6889ae9d484eed2876868209e33eb262511d.tar.gz";
+  }) {};
+in
+  pkgs.mkShellNoCC {
+    packages = [pkgs.typst];
+  }
